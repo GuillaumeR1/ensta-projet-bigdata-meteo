f75a7152146d3d536c47d1aeaa032b405a145430
GuillaumeR1 <robin.guillaume@gmx.com>
2026-03-10 19:25:55 +0100

    Correction du test de format de donnée

--- src/test/java/bigdatameteo/TestDataFormat.java
+++ src/test/java/bigdatameteo/TestDataFormat.java
@@ -15,17 +15,34 @@ public class TestDataFormat {
                 .master("local[*]")
                 .getOrCreate();
 
-        try {
 
+        spark.sparkContext().setLogLevel("ERROR");
+
+        try {
             String inputPath = "tmp/data/*.csv.gz";
 
-            Dataset<Row> dfFormate = Main.runPipeline(spark, inputPath);
+            // Données brutes
+            Dataset<Row> dfBrut = spark.read()
+                    .option("header", "true")
+                    .option("sep", ";")
+                    .schema(DataSchema.csvSchema())
+                    .csv(inputPath);
 
+            // Données formatées
+            Dataset<Row> dfFormate = DataFormatage.formater(dfBrut);
+
+            System.out.println("\n***** Schéma du jeu de donnée");
             dfFormate.printSchema();
+
+            System.out.println("\n***** 10 première lignes des données brutes :");
+            dfBrut.show(10, false);
+
+            System.out.println("\n***** 10 première lignes des données formatées :");
             dfFormate.show(10, false);
 
             long count = dfFormate.count();
 
+            System.out.println("\n***** Nombre de ligne totales :");
             System.out.println("Nombre total de lignes : " + count);
 
         } finally {
